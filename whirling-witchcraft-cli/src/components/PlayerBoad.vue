30773ad589cd90fea4fde47232d7ef0d94debd8c
watao-o <thbc.watao@gmail.com>
2023-09-19 23:53:40 +0900

    add:資材置き場サンプル作成

--- whirling-witchcraft-cli/src/components/PlayerBoad.vue
+++ whirling-witchcraft-cli/src/components/PlayerBoad.vue
@@ -28,33 +28,117 @@
             </v-card-title>
           </v-card-item>
           <v-card-text>
-            <v-card color="rgb(243, 247, 204)">
+            <v-card color="lime-lighten-5">
               <v-card-title>
                 {{card.name }}
               </v-card-title>
               <v-card-text color="white">
-                <v-chip><v-icon color="black">mdi-emoticon-devil</v-icon></v-chip>
-                <v-chip><v-icon color="white">mdi-rabbit</v-icon></v-chip>
-                <v-chip><font-awesome-icon :icon="['fas', 'frog']" style="color: green;"/></v-chip>
-                <v-chip><v-icon color="red">mdi-mushroom</v-icon></v-chip>
-                <v-chip><v-icon color="blue">mdi-spider</v-icon></v-chip>
+                <v-chip label variant="elevated" color="grey-darken-3"><v-icon color="black">mdi-emoticon-devil</v-icon></v-chip>
+                <v-chip label variant="elevated" color="grey-lighten-3"><v-icon color="white">mdi-rabbit</v-icon></v-chip>
+                <v-chip label variant="elevated" color="green-lighten-3"><font-awesome-icon :icon="['fas', 'frog']" style="color: green;"/></v-chip>
+                <v-chip label variant="elevated" color="red-lighten-3"><v-icon color="red">mdi-mushroom</v-icon></v-chip>
+                <v-chip label variant="elevated" color="blue-lighten-3"><v-icon color="blue">mdi-spider</v-icon></v-chip>
               </v-card-text>
               <v-card-text>
                 <v-icon color="black">mdi-arrow-down-thick</v-icon>
                 <v-icon color="black">mdi-swap-vertical</v-icon>
               </v-card-text>
               <v-card-text>
-                <v-chip><v-icon color="black">mdi-emoticon-devil</v-icon></v-chip>
-                <v-chip><v-icon color="white">mdi-rabbit</v-icon></v-chip>
-                <v-chip><font-awesome-icon :icon="['fas', 'frog']" style="color: green;"/></v-chip>
-                <v-chip><v-icon color="red">mdi-mushroom</v-icon></v-chip>
-                <v-chip><v-icon color="blue">mdi-spider</v-icon></v-chip>
+                <v-chip label variant="elevated" color="grey-darken-3"><v-icon color="black">mdi-emoticon-devil</v-icon></v-chip>
+                <v-chip label variant="elevated" color="grey-lighten-3"><v-icon color="white">mdi-rabbit</v-icon></v-chip>
+                <v-chip label variant="elevated" color="green-lighten-3"><font-awesome-icon :icon="['fas', 'frog']" style="color: green;"/></v-chip>
+                <v-chip label variant="elevated" color="red-lighten-3"><v-icon color="red">mdi-mushroom</v-icon></v-chip>
+                <v-chip label variant="elevated" color="blue-lighten-3"><v-icon color="blue">mdi-spider</v-icon></v-chip>
               </v-card-text>
             </v-card>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
+    <v-card color="indigo-lighten-1" class="mt-5" width="800">
+      <v-card-text class="pt-16">
+        <!-- 黒 -->
+        <v-row justify="center">
+          <v-col cols="1">
+            <v-chip><v-icon color="black">mdi-emoticon-devil</v-icon></v-chip>
+          </v-col>
+          <v-col v-for="n in 3" :key="n" cols="1">
+            <v-chip label variant="elevated" color="grey-darken-3">
+              <v-card
+                v-show="true"
+                height="15"
+                width="15"
+                color="black"
+              ></v-card>
+            </v-chip>
+          </v-col>
+        </v-row>
+        <!-- 白 -->
+        <v-row justify="center">
+          <v-col cols="1">
+            <v-chip><v-icon color="white">mdi-rabbit</v-icon></v-chip>
+          </v-col>
+          <v-col v-for="n in 4" :key="n" cols="1">
+            <v-chip label variant="elevated" color="grey-lighten-3">
+              <v-card
+                v-show="true"
+                height="15"
+                width="15"
+                color="white"
+              ></v-card>
+            </v-chip>
+          </v-col>
+        </v-row>
+        <!-- 赤 -->
+        <v-row justify="center">
+          <v-col cols="1">
+            <v-chip><v-icon color="red">mdi-mushroom</v-icon></v-chip>
+          </v-col>
+          <v-col v-for="n in 9" :key="n" cols="1">
+            <v-chip label variant="elevated" color="red-lighten-3">
+              <v-card
+                v-show="true"
+                height="15"
+                width="15"
+                color="red"
+              ></v-card>
+            </v-chip>
+          </v-col>
+        </v-row>
+        <!-- 青 -->
+        <v-row justify="center">
+          <v-col cols="1">
+            <v-chip><v-icon color="blue">mdi-spider</v-icon></v-chip>
+          </v-col>
+          <v-col v-for="n in 9" :key="n" cols="1">
+            <v-chip label variant="elevated" color="blue-lighten-3">
+              <v-card
+                v-show="true"
+                height="15"
+                width="15"
+                color="blue"
+              ></v-card>
+            </v-chip>
+          </v-col>
+        </v-row>
+        <!-- 緑 -->
+        <v-row justify="center">
+          <v-col cols="1">
+            <v-chip><font-awesome-icon :icon="['fas', 'frog']" style="color: green;"/></v-chip>
+          </v-col>
+          <v-col v-for="n in 9" :key="n" cols="1">
+            <v-chip label variant="elevated" color="green-lighten-3">
+              <v-card
+                v-show="true"
+                height="15"
+                width="15"
+                color="green"
+              ></v-card>
+            </v-chip>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
