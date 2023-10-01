afb6fff1d0a959fae95b39f3757ebbd0848b9748
watao-o <thbc.watao@gmail.com>
2023-10-01 21:47:16 +0900

    add:魔女カード取得関数追加

--- whirling-witchcraft-cli/src/utils/utils.js
+++ whirling-witchcraft-cli/src/utils/utils.js
@@ -1,8 +1,17 @@
 import data from '@/assets/data.json'
+
 /**
  * 調合法カードデータ取得
  * @param { String } cardId カードID
  */
 export function getMixingCardData(cardId) {
   return data.cardList.find(card => card.cardId === cardId)
+}
+
+/**
+ * 魔女カードデータ取得
+ * @param { String } witchId 魔女ID
+ */
+export function getWitchCardData(witchId) {
+  return data.witchList.find(witch => witch.witchId === witchId)
 }
